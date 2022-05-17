9714e2c708dc3d5c1fbcb44af11cab690346869e
Federico Buinosquy <fbuinosquy1985>
2022-05-17 14:25:44 -0300

    wip

--- update_cdn.sh
+++ update_cdn.sh
@@ -10,7 +10,8 @@ CDN_ID=$1            #E3554BHOW3RXY2
 DOMAIN_NAME=$2       #"aac5e1e3235cc4c028de730c26369163-d8052e4acdbbae74.elb.us-east-1.amazonaws.com"
 SHORT_DOMAIN_NAME=$3 #crm-uat-prod
 
-export PATH=$PATH:/usr/local/bin
+export PATH=/home/ubuntu/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
+apk add --update-cache python3 python3-dev py-pip build-base curl
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3 get-pip.py
 pip install awscli --upgrade
 /usr/local/bin/aws
