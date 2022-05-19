657b663816322037612a2715a35162efbd01056b
Federico Buinosquy <fbuinosquy1985>
2022-05-19 08:32:08 -0300

    wip

--- main.tf
+++ main.tf
@@ -9,5 +9,15 @@ resource "null_resource" "update_cdn" {
     interpreter = ["sh", "-c"]
     command     = "./update_cdn.sh ${var.cdn_id} ${var.lb_url} ${var.cdn_path}"
   }
+
+  provisioner "local-exec" {
+    when        = "destroy"
+    interpreter = ["sh", "-c"]
+    command     = "./remove_cdn.sh ${var.cdn_id} ${var.cdn_path}"
+  }
+
+  lifecycle {
+    create_before_destroy = false
+  }
 }
 
---
+++ remove_cdn.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+#====================================================================================================================
+#Script that remove a origin and behavior to a cloudfront distribution , only for Load Balancers for the moment
+#====================================================================================================================
+: ${1?"How to use: $0 CLOUDFRONT_ID DOMAIN_PATH"}
+: ${2?"How to use: $0 CLOUDFRONT_ID DOMAIN_PATH"}
+
+CDN_ID=$1            #E3554BHOW3RXY2
+SHORT_DOMAIN_NAME=$2 #crm-uat-prod
+
+function fail {
+  echo $1 >&2
+  exit 1
+}
+
+function retry {
+  local n=1
+  local max=3
+  local delay=5
+  while true; do
+    "$@" && break || {
+      if [[ $n -lt $max ]]; then
+        ((n++))
+        echo "================================"
+        echo "Command failed. Attempt $n/$max:"
+        echo "================================"
+        sleep $delay;
+      else
+        fail "The command has failed after $n attempts."
+      fi
+    }
+  done
+}
+
+rm -f cdn2.json
+echo "============================================================================================================="
+echo "OBTAINING CLOUDFRONT CONFIGURATION                                                                           "
+echo "============================================================================================================="
+retry aws cloudfront get-distribution-config --id ${CDN_ID} > cdn.json
+ETAG=$(cat cdn.json | jq -r .ETag)
+CDN=$(cat cdn.json | jq -r 'del(.ETag)')
+SHORT_DOMAIN_NAME_ORG=$SHORT_DOMAIN_NAME-org
+
+echo "============================================================================================================="
+echo "REMOVING THE ORIGIN FROM THE CONFIGURATION                                                                  "
+echo "============================================================================================================="
+
+CDN=$(echo $CDN | jq --arg RemoveOrigin "$SHORT_DOMAIN_NAME_ORG" 'del(.DistributionConfig.Origins.Items[] | select(.Id == $RemoveOrigin))')
+CDN=$(echo $CDN | jq '.DistributionConfig.Origins.Quantity -=1')
+
+echo "============================================================================================================="
+echo "REMOVING THE NEW BEHAVIOR FROM THE CONFIGURATION                                                                "
+echo "============================================================================================================="
+
+CDN=$(echo $CDN | jq --arg RemoveOrigin "$SHORT_DOMAIN_NAME_ORG" 'del(.DistributionConfig.CacheBehaviors.Items[] | select(.TargetOriginId == $RemoveOrigin))')
+CDN=$(echo $CDN | jq '.DistributionConfig.CacheBehaviors.Quantity -=1')
+
+echo "============================================================================================================="
+echo "APPLYING THE  CONFIGURATION                                                                                  "
+echo "============================================================================================================="
+echo $CDN |jq -r .DistributionConfig > cdn2.json
+result=$(retry aws cloudfront update-distribution --id $CDN_ID --distribution-config file://cdn2.json --if-match ${ETAG})		
+
+echo "============================================================================================================="
+echo "WAITING TO SEE IF THE CONFIGURATION WAS APPLIED                                                              "
+echo "============================================================================================================="
+echo "$(echo $result | jq -r '.Distribution.Id + ": " + .Distribution.Status')"
+aws cloudfront wait distribution-deployed --id $CDN_ID && echo "Cloudfront Modification Completed";
+
+rm -f cdn2.json
